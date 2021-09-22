e00e255dfa00ec9b13a7bdc5817072fb05d171bc
Padredilg <padredilg@knights.ucf.edu>
2021-09-22 19:34:56 -0400

    created overall skeleton

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(routes);
+
+//Use Handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+});
