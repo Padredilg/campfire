d39895172ce3c6d747acd6d661b478a325adc829
Aidan Amato <aidanamato@comcast.net>
2021-09-22 23:42:13 -0400

    add get route to user-routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./home-routes.js');
+const htmlRoutes = require('./html');
+const apiRoutes = require('./api')
 
-router.use('/', homeRoutes);
+router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {//Catch-all route for wrong searches!
-  res.send("<h1>Wrong Route!</h1>")
+  res.status(404).send("<h1>Wrong Route!</h1>");
   //try to redirect back to homepage
 });
 
