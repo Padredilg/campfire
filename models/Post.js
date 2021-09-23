acdc4cda749799ce6917d1abeb9b28ec49fe6bf1
Aidan Amato <aidanamato@comcast.net>
2021-09-22 21:41:21 -0400

    add Friendship model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+
+}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    picture: {
+      type: DataTypes.BLOB,
+      allowNull: true,
+    },
+    likes: {
+      type: DataTypes.STRING,
+      defaultValue: 0
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = Post;
