5ba3e7d0e9bba122e63ffa3f262e7b1699efcc8b
Michael Astalos <michaeldont@gmail.com>
2021-09-23 22:30:55 -0400

    able to detect when user is connected to chat

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3001;
 const sess = {
     secret: 'Super secret secret',
@@ -29,6 +27,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 app.use(routes);
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+    });
+  });
+
 // Use Handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
@@ -36,12 +44,8 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
   
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+    http.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
