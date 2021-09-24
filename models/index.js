aac1bcdcdfc9d27461dda43163625b4e67d405b4
Padredilg <padredilg@knights.ucf.edu>
2021-09-24 08:25:33 -0400

    updated associations to include Like model and Comment model

--- models/index.js
+++ models/index.js
@@ -1,21 +1,56 @@
 // require models
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require("./Comment");
+const Like = require("./Like");
 const Friendship = require('./Friendship');
 
-// associations
+// USER-POST ASSOCIATIONS
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
-
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+// USER-COMMENT ASSOCIATIONS
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// POST-COMMENT ASSOCIATIONS
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+//USER-LIKE-POST ASSOCIATIONS
+User.hasMany(Like, {
+  foreignKey: 'user_id'
+});
+Post.hasMany(Like, {
+  foreignKey: 'post_id'
+});
+User.belongsToMany(Post, {
+  through: Like,
+  as: 'liked_posts',
+  foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+  through: Like,
+  as: 'liked_posts',
+  foreignKey: 'post_id'
+});
+
+
 User.hasMany(Friendship, {
   foreignKey: 'requesting_user_id'
 });
-
 Friendship.belongsTo(User, {
   foreignKey: 'requesting_user_id'
 });
@@ -23,10 +58,10 @@ Friendship.belongsTo(User, {
 User.hasMany(Friendship, {
   foreignKey: 'requested_user_id'
 });
-
 Friendship.belongsTo(User, {
   foreignKey: 'requested_user_id'
 });
+
 
 module.exports = {User, Post, Friendship};
 
