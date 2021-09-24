bf0c58aed6508d4432c8d995f7378ea2c37599d8
Padredilg <padredilg@knights.ucf.edu>
2021-09-24 09:22:33 -0400

    created and built posts routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,164 @@
+const router = require('express').Router();
+const { Post, User, Like, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// http://localhost:3001/api/posts
+/* UPDATE LOGOUT AND DELETE WILL NEED withAuth for security reasons we must add them after the test phase is done*/
+
+// GET all posts
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        // Query configuration
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+            'user_id',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']//this property displays the like_count
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+});
+
+//GET post by ID
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+          ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//POST create new post
+router.post('/', withAuth, (req, res) => {
+    console.log("POST", req);
+    // expects {title: 'Taskmaster goes public!', content: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT - like (or dislike) a post
+//needs to be before PUT by /:id, or express will think uplike is an id param
+router.put('/uplike', withAuth, (req, res) => {
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.uplike({ ...req.body, user_id: req.session.user_id }, { Like, Comment, User })
+            .then(updatedLikeData => res.json(updatedLikeData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+});
+
+//PUT update a post (title and content)
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//DELETE a post
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,8 +3,7 @@ const { User, Post, Like, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // http://localhost:3001/api/users
-/* UPDATE LOGOUT AND DELETE WILL NEED withAuth for security reasons
-We must add them after the test phase is done*/
+/* UPDATE LOGOUT AND DELETE WILL NEED withAuth for security reasons we must add them after the test phase is done*/
 
 // GET all users
 router.get('/', (req, res) => {
