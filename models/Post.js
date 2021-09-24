784a30fae72bbeb5169dab08ee2ead5bb9f377d3
Padredilg <padredilg@knights.ucf.edu>
2021-09-24 08:15:05 -0400

    added like/unlike logic to Post.js, and removed like prop

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,46 @@ const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Post extends Model {
-
+  static uplike(body, models) {
+    return models.Like.findOne({
+        where: {
+            user_id: body.user_id,
+            post_id: body.post_id
+        }
+    }).then(foundLike => {
+        if (foundLike) {
+            // If the Like was found, destroy it - "unlike"
+            return models.Like.destroy({
+                where: {
+                    user_id: body.user_id,
+                    post_id: body.post_id
+                }
+            });
+        } else {
+            // If the like was not found, create a new like
+            return models.Like.create({
+                user_id: body.user_id,
+                post_id: body.post_id
+            });
+        }
+    }).then(() => {
+        return Post.findOne({
+            where: {
+                id: body.post_id
+            },
+            attributes: [
+                "id",
+                "content",
+                "title",
+                "created_at",
+                [
+                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                    "vote_count"
+                ]
+            ]
+        });
+    });
+  }
 }
 
 Post.init(
@@ -20,10 +59,6 @@ Post.init(
     picture: {
       type: DataTypes.BLOB,
       allowNull: true,
-    },
-    likes: {
-      type: DataTypes.STRING,
-      defaultValue: 0
     },
     user_id: {
       type: DataTypes.INTEGER,
