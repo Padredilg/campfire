27d2bed3d0bd40176abbf106c8a1308b8c606f79
Padredilg <padredilg@knights.ucf.edu>
2021-09-25 08:54:38 -0400

    added homepage route and post rendering to homepage

--- controllers/html/home-routes.js
+++ controllers/html/home-routes.js
@@ -4,11 +4,56 @@ const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        // posts,
-        // loggedIn: req.session.loggedIn
+    console.log(req.session);
+    Post.findAll({
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+            'user_id',
+            [sequelize.literal('(SELECT COUNT(*) FROM love WHERE post.id = love.post_id)'), 'love_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-})
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => {
+            post = post.get({ plain: true })
+
+            if(post.user_id === req.session.user_id){
+                post.edit = true;
+            }
+            else{
+                post.edit = false;
+            }
+            return post;
+        });
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+            username: req.session.username
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
