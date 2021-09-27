1a0e136c5b590b20e3f2ea1d0d54c309cca92c67
Aidan Amato <aidanamato@comcast.net>
2021-09-26 20:44:25 -0400

    working on changing profile get route to get user first

--- controllers/html/profile-routes.js
+++ controllers/html/profile-routes.js
@@ -3,59 +3,77 @@ const sequelize = require('../../config/connection');
 const {Post, User, Comment} = require('../../models');
 
 router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-        'id',
-        'content',
-        'title',
-        'created_at',
-        'user_id',
-        [sequelize.literal('(SELECT COUNT(*) FROM love WHERE post.id = love.post_id)'), 'love_count']
-    ],
-    include: [
-        {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-        },
-        {
-            model: User,
-            attributes: ['id', 'username', 'bio', 'img_url'],
-            where: {
-              id: req.session.user_id
-            }
-        }
-    ]
-})
+  User.findOne({
+    // where: {id: req.session.user_id},
+    where: {id: 1},
+    attributes: ['id', 'username', 'bio', 'img_url'],
+    // include: [
+    //   {
+    //     model: Post,
+    //     attributes: [
+    //       'id',
+    //       'content',
+    //       'title',
+    //       'created_at',
+    //       'user_id',
+    //       [sequelize.literal('(SELECT COUNT(*) FROM love WHERE post.id = love.post_id)'), 'love_count']
+    //     ],
+    //     order: [['created_at', 'DESC']],
+    //     include: [
+    //       {
+    //         model: Comment,
+    //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //             include: {
+    //                 model: User,
+    //                 attributes: ['username']
+    //             }
+    //       }
+    //     ]
+    //   }
+    // ]
+  })
   .then(dbPostData => {
-    const posts = dbPostData.map(post => {
-      post = post.get({ plain: true })
+    res.json(dbPostData);
+    // const posts = dbPostData.map(post => {
+    //   post = post.get({ plain: true })
 
-      if(post.user_id === req.session.user_id){
-          post.edit = true;
-      }
-      else{
-          post.edit = false;
-      }
-      return post;
-    });
+    //   if(post.user_id === req.session.user_id){
+    //       post.edit = true;
+    //   }
+    //   else{
+    //       post.edit = false;
+    //   }
+    //   return post;
+    // });
 
-    const user = posts[0].user;
-    if (user.id === req.session.user_id) {
-      user.edit = true;
-    } else {
-      user.edit = false;
-    }
+    // let user;
+    // if (posts.length > 0) {
+    //   user = posts[0].user;
+    //   if (user.id === req.session.user_id) {
+    //     user.edit = true;
+    //   } else {
+    //     user.edit = false;
+    //   }
+    // } 
 
-    res.render('profile', {
-        user,
-        posts,
-        loggedIn: req.session.loggedIn,
-        username: req.session.username
-    });
+    // let renderOptions;
+    // if (user) {
+    //   renderOptions = {
+    //     user,
+    //     posts,
+    //     loggedIn: req.session.loggedIn,
+    //     username: req.session.username
+    //   }
+    // } else {
+    //   renderOptions = {
+    //     posts,
+    //     loggedIn: req.session.loggedIn,
+    //     username: req.session.username
+    //   }
+    // }
+    // console.log(renderOptions);
+
+    // res.render('profile', {renderOptions});
   });
 });
 
