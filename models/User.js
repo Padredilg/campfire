d3af7481f99c4d9efb2b2186755c3c601167efdb
Padredilg <padredilg@knights.ucf.edu>
2021-09-27 07:53:25 -0400

    commented bio and img out

--- models/User.js
+++ models/User.js
@@ -39,16 +39,17 @@ User.init(
         len: 4
       }
     },
-    bio: {
-      type: DataTypes.TEXT,
-    },
-    img_url: {
-      type: DataTypes.STRING,
-      defaultValue: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-      validate: {
-      isURL: true
-      }
-    }
+    // bio: {
+    //   type: DataTypes.TEXT,
+    // },
+    // img_url: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   defaultValue: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    //   validate: {
+    //   isURL: true
+    //   }
+    // }
   },
   {
     hooks: {
