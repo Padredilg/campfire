489424b744fc258899cbecf846f700ff6205f197
Padredilg <padredilg@knights.ucf.edu>
2021-09-27 16:20:41 -0400

    including hour and minute on time of post

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,26 @@
 module.exports = {
     format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-            date
-        ).getFullYear()}`;
+        const postTime = new Date(date);
+        let month = postTime.getMonth() + 1;
+        let day = postTime.getDate();
+        let year = postTime.getFullYear();
+        let hour = postTime.getHours();
+        let minutes = postTime.getMinutes();
+        let amPm = 'AM'
+
+        if(hour >= 13){
+            hour -= 12;
+            amPm = 'PM'
+        }
+
+        const today = new Date();
+        if(day == today.getDate() && month == today.getMonth()+1 && year == today.getFullYear()){
+            console.log('hello')
+            return `Today at ${hour}:${minutes} ${amPm}`;
+        }
+        else{
+            return `${month}/${day}/${year} at ${hour}:${minutes} ${amPm}`;
+        }
     },
     format_plural: (word, amount) =>{
         if(amount !== 1){
