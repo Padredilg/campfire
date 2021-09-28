456f774246631c7ce83909ad171e546a06910abb
Michael Astalos <michaeldont@gmail.com>
2021-09-27 23:18:01 -0400

    improved chat message

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,23 +1,37 @@
 const socket = io(); 
-let form = document.getElementById('form');
+let form = document.getElementById('meatballs');
 let input = document.getElementById('input');
 let messages = document.getElementById('messages');
 let hiddenUsername = document.getElementById('username');
+let channelBtns = document.querySelectorAll('.channelBtn');
 
 const username = hiddenUsername.textContent;
+
+function channelBtnHandler(e) {
+    e.preventDefault();
+    console.log(e)
+}
+
+channelBtns.forEach((element, index)  => {
+    element.onclick = (event) => {
+        let channelName = element.textContent;
+        socket.emit('new-user', channelName, username)
+    }
+});
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-    socket.emit('chat message', `${username}: ${input.value}`);
+    socket.emit('send-chat-message', username, input.value);
     input.value = '';
     }
 });
 
-socket.on('chat message', function(msg) {
+
+socket.on('chat-message', function(message) {
     var item = document.createElement('p');
     item.className = "m-1"
-    item.textContent = msg;
+    item.textContent = `${message.username}: ${message.msg}`;
     messages.appendChild(item);
-    console.log('message: '+ msg)
+    console.log('message: '+ message.msg)
 });
--- server.js
+++ server.js
@@ -29,16 +29,14 @@ app.use(routes);
 const rooms = {}
 
 io.on('connection', (socket) => {
-    socket.on('new-user', (name) => {
+    socket.on('new-user', (channelName, username) => {
+        socket.join(channelName)
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-    socket.on('chat message', msg => {
-       io.emit('chat message', msg);
-        console.log('message: '+ msg)
-        console.log(session.username)
-
+    socket.on('send-chat-message', (username, msg) => {
+       io.emit('chat-message', {username:username, msg:msg});
     });
   });
 
