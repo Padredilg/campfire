3f4c05a7f1c67616e98dd9ac676cb84732e50f1e
Padredilg <padredilg@knights.ucf.edu>
2021-09-29 16:53:28 -0400

    fixed page reload on edit-post page buttons

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -10,7 +10,7 @@ async function deleteFormHandler(event) {
     });
 
     if (response.ok) {
-        document.location.href = '/'; /* bjj - This used to be to '/homepage', but I think we need this to go to '/' so that the correct parameters will be passed to homepage */
+        document.location.href = '/posts-wall'; /* bjj - This used to be to '/homepage', but I think we need this to go to '/' so that the correct parameters will be passed to homepage */
     } 
     else {
         alert(response.statusText);
