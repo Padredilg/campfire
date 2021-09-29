e1133c19cc3214447a9966d652e2d017c7cafef6
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-09-29 15:40:12 -0400

    have chat submit on enter

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -54,6 +54,14 @@ formEl.addEventListener('submit', function(e) {
     }
 });
 
+function submitOnEnter(event){
+    if(event.which === 13){
+        event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
+        event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
+    }
+}
+
+newChatMessageEl.addEventListener("keypress", submitOnEnter);
 
 socket.on('chat-message', function(message) {
 
